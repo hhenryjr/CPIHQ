eeb83d39df671af6317ef6141265965869a3fbf8
jamesdarragh <jimdarragh@hotmail.com>
2016-08-28 15:18:19 -0700

    Added parseService

---
+++ CPI/CPI/Services/ParseService.cs
@@ -0,0 +1,27 @@
+﻿using Code7248.word_reader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CPI.Services
+{
+    public class ParseService
+    {
+        public static string ParseFileExtractor(string path)
+        {
+            string text = "";
+            try
+            {
+                TextExtractor extractor = new TextExtractor(path);
+                text = extractor.ExtractText(); //The string 'text' is now loaded with the text from the Word Document
+            }
+            catch (Exception e)
+            {
+                text = e.Message;
+            }
+
+            return text;
+        }
+    }
+}
