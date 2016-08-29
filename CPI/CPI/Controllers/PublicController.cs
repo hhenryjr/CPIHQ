28051a5a725300421ea35e1b44cab35dd12859f3
hhenryjr <h2rnng@yahoo.com>
2016-08-28 19:19:32 -0700

    implemented MVC attribute routing

--- CPI/CPI/Controllers/PublicController.cs
+++ CPI/CPI/Controllers/PublicController.cs
@@ -1,20 +1,19 @@
 ﻿using CPI.Models.ViewModels;
 using CPI.Services;
 using System;
-using System.Web.Http;
 using System.Web.Mvc;
 
 namespace CPI.Controllers
 {
-    [System.Web.Http.AllowAnonymous]
+    [AllowAnonymous]
     public class PublicController : BaseController
     {
         //public PublicController(ISiteConfig config) : base(config)
         //{ }
 
         // GET: Public
-        [System.Web.Http.Route("~/Login")]
-        [System.Web.Http.Route("~/")]
+        [Route("~/Login")]
+        [Route("~/")]
         public ActionResult Login()
         {
             return View();
@@ -27,13 +26,13 @@ namespace CPI.Controllers
         //    return RedirectToAction("Index", "Home");
         //}
 
-        [System.Web.Http.Route("~/Register")]
+        [Route("~/Register")]
         public ActionResult Register()
         {
             return View();
         }
         
-        [System.Web.Http.Route("~/Confirm/{token:guid}")]
+        [Route("~/Confirm/{token:guid}")]
         public ActionResult ConfirmEmail(Guid token)
         {
             ItemViewModel<Guid> model = new ItemViewModel<Guid>();
@@ -41,13 +40,13 @@ namespace CPI.Controllers
             return View(model);
         }
 
-        [System.Web.Http.Route("~/ForgotPassword")]
+        [Route("~/ForgotPassword")]
         public ActionResult ForgotPassword()
         {
             return View();
         }
 
-        [System.Web.Http.Route("~/ResetPassword/{token:guid}")]
+        [Route("~/ResetPassword/{token:guid}")]
         public ActionResult ResetPassword(Guid token)
         {
             return View(token);
